662d36f13173373f12eb42a9a2c8dc8154f45885
Chef Tech <cheftech@patraldo.com>
2024-03-06 19:06:35 -0600

    remove skeleton and start customization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Patraldo Dot Dev</h1>
+<p>A staging ground for web app development</p>
