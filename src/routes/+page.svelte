8db2b26306dfb2f58763b9798f2e8abae86de71d
Chef Tech <cheftech@patraldo.com>
2024-03-08 11:09:18 -0600

    following Svelte tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Patraldo Dot Dev</h1>
+<script>
+	let name = 'Christophe';
+import img from '$lib/image.gif';
+</script>
+<h1>Welcome, {name.toUpperCase()}, to Patraldo Dot Dev</h1>
 <p>A staging ground for web app development</p>
+
+<img src={img} alt='{name} moves slowly, like a zombie.' />
