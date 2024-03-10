9cb12766f2c13af0ec9e13042f9a8d58da153d23
Chef Tech <cheftech@patraldo.com>
2024-03-10 13:45:34 -0600

    add video mp4 with audio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
      let name = 'Christophe';
-     import myVideo from '$lib/videoParis.mp4';
 </script>
 
 
 <h1>Welcome, {name.toUpperCase()}, to Patraldo Dot Dev</h1>
 <p>A staging ground for web app development</p>
 
-<video src="{myVideo}" alt='{name} dances up a storm' autoplay loop />
+<video src="https://storage.googleapis.com/patraldo/patraldo-dev/bg-paris300x400-x-out.mp4" alt='{name} dances up a storm' autoplay loop />
 
