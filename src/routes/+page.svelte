fecdd9d125b2112df13d3ec4586ea45663e94831
Chef Tech <cheftech@patraldo.com>
2024-03-10 13:55:18 -0600

    add video mp4 with audio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,6 @@
 <h1>Welcome, {name.toUpperCase()}, to Patraldo Dot Dev</h1>
 <p>A staging ground for web app development</p>
 
-<video src="https://storage.googleapis.com/patraldo/patraldo-dev/bg-paris300x400-x-out.mp4" alt='{name} dances up a storm' autoplay loop />
+
+<video src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4" alt='{name} dances up a storm' muted autoplay loop />
 
