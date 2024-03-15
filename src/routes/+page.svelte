d6df88dfb50291ac69dfb21047d17c4b49726786
Chef Tech <cheftech@patraldo.com>
2024-03-15 14:20:53 -0600

    eliminate pinky png and caminandes mp4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,5 +8,27 @@
 <p>A staging ground for web app development</p>
 
 
+<div class="warning">
 <video src="https://storage.googleapis.com/patraldo/patraldo-dev/bg-paris300x400-x-out.mp4" alt='{name} dances up a storm' muted autoplay loop controls width="620" />
+</div
+
+<style>
+.warning {
+  border: 10px ridge #f00;
+  background-color: #ff0;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+}
+
+.warning img {
+  width: 100%;
+}
+
+.warning p {
+  font: small-caps bold 1.2rem sans-serif;
+  text-align: center;
+}
+
+</style>
 
