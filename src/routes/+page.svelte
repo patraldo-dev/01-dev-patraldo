9538cb930fd4413a35395124d7162da7f595e2d0
Chef Tech <cheftech@patraldo.com>
2024-03-16 09:32:14 -0600

    add preload=metadata for thumbnail to video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,22 @@
 <p>A staging ground for web app development</p>
 
 
-<div class="warning">
-<video src="https://storage.googleapis.com/patraldo/patraldo-dev/bg-paris300x400-x-out.mp4#t=0.1" alt='{name} dances up a storm' muted autoplay loop controls preload="metadata" width="620" />
+<div class="greenMonster">
+<video preload=metadata src="https://storage.googleapis.com/patraldo/patraldo-dev/bg-paris300x400-x-out.mp4#t=0.1" alt='{name} dances up a storm' muted loop controls width="620" />
 <p> <a href="https://antoine.patraldo.com">Artiste: Antoine Patraldo</a></p> <p><a href="https://pinchepoutine.digital">Animación: ¡Pinche Poutine! Digital </a></p>
 </div>
 
 <style>
-.warning {
-  border: 10px ridge #f00;
-  background-color: #ff0;
+.greenMonster {
+  border: 10px ridge #fff;
+  background-color: #ccc;
   padding: 0.5rem;
   display: flex;
   flex-direction: column;
 }
 
 
-.warning p {
+.greenMonster p {
   font: small-caps bold 5.2rem sans-serif;
   text-align: center;
 }
